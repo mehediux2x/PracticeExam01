d31d327213255f02a68bac7d5158bdf71ad9d03e
mehediux2x <mehediux2x@yahoo.com>
2019-08-29 17:35:10 -0400

    4th updates

--- UseArrayList.java
+++ UseArrayList.java
@@ -1,4 +1,40 @@
 package classExam001;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class UseArrayList {
+
+        public static void main(String[] args) {
+
+            ArrayList<String> list = new ArrayList<String>();
+
+            list.add("roni");
+            list.add("Raw");
+            list.add("shell");
+            list.add("arikon");
+            list.add("rool");
+            list.add("rock");
+
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.add("jarfer");
+            linkedList.add("goh");
+            linkedList.add("jukker");
+            linkedList.add("dowrin");
+            linkedList.add("purno");
+
+
+            System.out.println(linkedList.peek());
+
+            list.remove(4);
+
+            Iterator fh = list.iterator();
+            while (fh.hasNext()) {
+                System.out.println(fh.next());
+
+            }
+
+        }
 }
--- UseMap.java
+++ UseMap.java
@@ -1,4 +1,31 @@
 package classExam001;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 public class UseMap {
+    public static void main(String[] args) {
+
+
+        HashMap<String, Integer> studentDetails = new HashMap<String, Integer>();
+
+        studentDetails.put("sakib", 16);
+        studentDetails.put("rk", 23);
+        studentDetails.put("iftekar", 22);
+        studentDetails.put("jabed",13);
+
+        for (String i : studentDetails.keySet()) {
+            System.out.println("name of the person: " + i + " age: " + studentDetails.get(i));
+        }
+        HashMap<String, List<String>> list = new HashMap<String, List<String>>();
+
+        Iterator tr = studentDetails.entrySet().iterator();
+        while (tr.hasNext()){
+            System.out.println(tr.next());
+        }
+
+
+    }
+
 }
